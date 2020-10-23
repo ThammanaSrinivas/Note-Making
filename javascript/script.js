f21cf053622642fa306cd69a6dc8a8a56e424a41
ThammanaSrinivas <sreenivast84@gmail.com>
2020-10-23 23:50:09 +0530

    Save function is successfully implemented

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
-let createNewNote = function() {
+let createNewNote = function(event=null,head='',body='') {
+  // for add button 'click' (mouseClick Object) is 1st parameter hence event is extremely important 
+  // even though it is not used
 
   let newNote = document.createElement('div');
   newNote.innerHTML = `
@@ -11,13 +13,18 @@ let createNewNote = function() {
       </div>
       <div class="noteHead"> 
         <textarea class="noteHeadInput"></textarea> 
-      </div> <div class="noteBody"> 
+      </div> 
+      <div class="noteBody"> 
         <textarea class="noteBodyInput"></textarea> 
       </div> 
     </div>
   `;
 
   bottomContainer = document.querySelector(".bottom-container");
+  let noteHeadInput = newNote.querySelector(".noteHeadInput");
+  let noteBodyInput = newNote.querySelector(".noteBodyInput");
+  noteHeadInput.value = head;
+  noteBodyInput.value = body;
   bottomContainer.appendChild(newNote);
 
   let closeButton = newNote.querySelector(".close-btn");
@@ -26,5 +33,32 @@ let createNewNote = function() {
   });
 };
 
+let saveNotes = function() {
+  let notesArray = document.querySelectorAll(".note");
+  let length = notesArray.length;
+  for(let i=0;i<length;++i) {
+    let noteHeadInput = notesArray[i].querySelector(".noteHeadInput"); 
+    let noteBodyInput = notesArray[i].querySelector(".noteBodyInput"); 
+    localStorage.setItem("head"+i,noteHeadInput.value);
+    localStorage.setItem("body"+i,noteBodyInput.value); 
+  }
+  // to remove previously saved notes
+  for(let i=length;;++i) {
+    if(localStorage.getItem("head"+i)==null)  break;
+    localStorage.removeItem("head"+i);
+    localStorage.removeItem("body"+i);
+  }
+  alert("successful");
+}
+
+//load notes
+for(let i=0;;++i) {
+  if(localStorage.getItem("head"+i)==null)  break;
+  createNewNote(null,localStorage.getItem("head"+i),localStorage.getItem("body"+i));  
+}
+
 let add = document.querySelector(".add-btn");
 add.addEventListener('click',createNewNote);
+
+let save = document.querySelector(".save-btn");
+save.addEventListener('click',saveNotes);
